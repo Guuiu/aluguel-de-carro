ec28d68dbfdcc9700b31a389383b5943ea661556
guuiu <gui-keiroz@live.com>
2018-08-31 12:22:56 -0300

    criar subir o site aplicando backend

---
+++ index.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const expressMongoDb = require('express-mongo-db');
+
+const app = express();
+app.set('view engine', 'ejs');
+app.use('/static', express.static('static'));
+app.use(bodyParser.urlencoded());
+app.use(expressMongoDb('mongodb://guilherme:gui1234@ds239412.mlab.com:39412/carjhonson'));
+
+app.get('', (req, res) => {
+    res.render('index');
+});
+
+app.post('', (req, res) => {
+    req.db.collection('mensagens').insert(req.body, (erro) => {
+        console.log(erro);
+        res.render('obrigado');
+    });
+});
+
+app.get('/gatos', (req, res) => {
+    req.db.collection('gatos').find().toArray((erro, dados) => {
+        res.render('gatos', {'gatos': dados});
+    });
+});
+
+app.get('/sobre', (req, res) => {
+    res.render('sobre');
+});
+
+app.get('/admin/mensagens', (req, res) => {
+    req.db.collection('mensagens').find().toArray((erro, dados) => {
+        res.render('admin-mensagens', {'mensagens': dados});
+    });
+});
+
+app.get('/admin/gatos', (req, res) => {
+    req.db.collection('gatos').find().toArray((erro, dados) => {
+        res.render('admin-gatos', {'gatos': dados});
+    });
+});
+
+app.get('/admin/gatos/inserir', (req, res) => {
+    res.render('admin-gatos-inserir');
+});
+
+app.post('/admin/gatos/inserir', (req, res) => {
+    req.db.collection('gatos').insert(req.body, (erro) => {
+        console.log(erro);
+        res.render('admin-gatos-inserir');
+    });
+});
+
+app.get('/contato', (req, res) => {
+    res.render('contato');
+});
+
+
+app.post('/contato', (req, res) => {
+    req.db.collection('contato').insert(req.body, (erro) => {
+        console.log(erro);
+        res.render('contato');
+    });
+});
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log('Servidor inicializado')
+});
